ace416709659ffbac117703d37b5a4503ec5daf6
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2026-04-08 15:23:22 -0700

    course done

--- mcp_client.py
+++ mcp_client.py
@@ -4,7 +4,9 @@ from typing import Optional, Any
 from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
-
+from pydantic import AnyUrl
+if sys.platform == "win32" and sys.version_info < (3, 14):
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
 class MCPClient:
     def __init__(
@@ -43,13 +45,13 @@ class MCPClient:
 
     async def list_tools(self) -> list[types.Tool]:
         # TODO: Return a list of tools defined by the MCP server
-        return []
-
+        result = await self.session().list_tools()
+        return result.tools
     async def call_tool(
         self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
         # TODO: Call a particular tool and return the result
-        return None
+        return await self. session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
@@ -61,7 +63,12 @@ class MCPClient:
 
     async def read_resource(self, uri: str) -> Any:
         # TODO: Read a resource, parse the contents and return it
-        return []
+        result = await self.session().read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+        if isinstance(resource, types.ResourceContents):
+            if resource.mime_type == "application/json":
+                return json.loads(resource.text)
+            return resource.text
 
     async def cleanup(self):
         await self._exit_stack.aclose()
@@ -82,7 +89,8 @@ async def main():
         command="uv",
         args=["run", "mcp_server.py"],
     ) as _client:
-        pass
+        result = await _client.list_tools()
+        print(result)
 
 
 if __name__ == "__main__":
--- mcp_server.py
+++ mcp_server.py
@@ -1,5 +1,5 @@
 from mcp.server.fastmcp import FastMCP
-
+from pydantic import Field
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
 
@@ -13,9 +13,50 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool(
+    name = "read_doc_contents",
+    description = "Read the contents of a document and return it as a string.",
+)
+def read_documents(
+        doc_id: str = Field(description="The id of the document to read.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found.")
+    return docs[doc_id]
+
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name = "edit_doc_contents",
+    description = "Edit the contents of a document and return the updated contents as a string.",
+)
+def edit_document(
+    doc_id: str = Field(description="The id of the document to edit."),
+    old_contents: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_contents: str = Field(description="The text to replace the old text with."),
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found.")
+    if old_contents not in docs[doc_id]:
+        raise ValueError(f"Old contents not found in doc {doc_id}.")
+    updated_contents = docs[doc_id].replace(old_contents, new_contents)
+    docs[doc_id] = updated_contents
+    return updated_contents
 # TODO: Write a resource to return all doc id's
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_docs():
+    return list(docs.keys())
 # TODO: Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def get_doc_contents(doc_id: str):
+    if(doc_id not in docs):
+        raise ValueError(f"Doc with id {doc_id} not found.")
+    return docs[doc_id]
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
